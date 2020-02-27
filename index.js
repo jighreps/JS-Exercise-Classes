486659c56bd7419ad729728939647325e0a0563e
jighreps <jipsga@gmail.com>
2020-02-27 15:51:11 -0800

    completed up to class instructor problem

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ class Airplane {
   }
 }
 
+
 /*
 // 👇 COMPLETE YOUR WORK BELOW 👇
 // 👇 COMPLETE YOUR WORK BELOW 👇
@@ -41,8 +42,27 @@ class Airplane {
 */
 
 class Person {
- //help me!
+  constructor(name, age){
+    this.name = name,
+    this.age = age,
+    this.stomach = [];
 }
+  eat(someFood){
+    if (this.stomach.length < 10){
+      return this.stomach.push(someFood);
+    }
+  }  
+    poop(){
+      return this.stomach = [];
+    }  
+
+    toString() {
+    return `${this.name}, ${this.age}`;
+    }
+  }
+  let newPerson = new Person('Biggles', 69);
+  console.log(newPerson.toString());
+
 
 /*
   TASK 2
@@ -59,8 +79,23 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon){
+    this.model = model,
+    this.milesPerGallon = milesPerGallon,
+    this.tank = 0,
+    this.odometer = 0;
+  }
 
+  fill(gallons){
+    return this.tank = this.tank + gallons;
+  }
+
+  drive(distance){
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank - (distance/this.milesPerGallon);
+  }  
 }
+
 
 /*
   TASK 3
@@ -75,7 +110,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(attributes){
+    this.name = attributes.name,
+    this.age = attributes.age,
+    this.location = attributes.location;
+  }
+    speak(attributes){
+      return `Hello, my name is ${this.name}, I live in ${this.location} `;
+  }
 }
 
 /*
@@ -92,9 +134,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+  constructor(attr){
+    super(attr)
+      this.specialty = attr.specialty,
+      this.favLanguage = attr.favLanguage,
+      this.catchPhrase = attr.catchPhrase
+    }
+    // demo(subject){
+    //   return 'Today we are learning `${subject}, `
+    // }
+  }
 
-}
+
 
 /*
   TASK 5
